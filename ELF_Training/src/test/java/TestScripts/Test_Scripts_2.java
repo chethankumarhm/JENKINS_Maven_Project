a202a262382343e74b7baee7b56aef4f2b147079
cheth <cheth@LAPTOP-CNDO9REH>
2022-12-07 11:57:45 +0530

    jenkins TestScript

---
+++ ELF_Training/src/test/java/TestScripts/Test_Scripts_2.java
@@ -0,0 +1,22 @@
+package TestScripts;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test_Scripts_2 {
+	public WebDriver driver;
+	 
+	@Test
+	public void honda() {
+		System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(25,TimeUnit.SECONDS);
+		driver.navigate().to("https://www.honda2wheelersindia.com/");
+		driver.close();
+		}
+	}
+
